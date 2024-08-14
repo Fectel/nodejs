f7314d55b523425ffb4a8c1b13371e10d6ca267e
Fectel <HectorGamador@gmail.com>
2024-08-14 17:12:16 +0000

    added soe=cket.io server

--- app.js
+++ app.js
@@ -5,11 +5,31 @@ const indexRouter = require('./routes/index');
 const app = express();
 const PORT = 3000;
 
+
+
+const fs = require('fs');
+const {createServer } = require('https');
+const { Server } = require("socket.io");
+const httpsServer = createServer({
+  key: fs.readFileSync('./key.pem'),
+  cert: fs.readFileSync('./cert.pem')
+},app);
+
+// socket.io working
+io.on("connection", (socket) => {
+
+  console.log("socket.io is connected")
+})
+
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Server running at {PORT}/`);
+// app.listen(PORT, () => {
+//   console.log(`Server running at {PORT}/`);
+// });
+httpsServer.listen(PORT, () => {
+  console.log(`Socket.io and Express Server running at {PORT}/`);
 });
