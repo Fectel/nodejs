df3181474ffa89902ba284c1b8243c144058eb21
Fectel <HectorGamador@gmail.com>
2024-08-15 18:07:58 +0000

    changed listens

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require('express');
-const path = require('path');
-const indexRouter = require('./routes/index');
 const fs = require('fs');
-const {createServer } = require('https');
 const { Server } = require("socket.io");
-// const io = require("socket.io");
 const https = require('https');
 
 const app = express()
@@ -18,37 +14,10 @@ const app = express()
 server.listen(3000)
 app.listen(server)
 
-// app.start = app.listen = function(){
-//   return server.listen.apply(server, arguments)
-// }
-
-console.log(app, "APPP");
-console.log(server, "server!!!!")
-
-
-// console.log(app, "<--- APPP")
-
-// const httpsServer = createServer({
-//   key: fs.readFileSync('./key.pem'),
-//   cert: fs.readFileSync('./cert.pem')
-// },app);
-// const io = new Server(httpsServer);
-
-
-// const PORT = 3000;
-
-// socket.io working
 io.on("connection", (socket) => {
 
   console.log("socket.io is connected")
 })
-// app.listen(PORT, () => {
-//   console.log(`Server running at {PORT}/`);
-// });
-// httpsServer.listen(PORT, () => {
-//   console.log(`Socket.io and Express Server running at ${PORT}`);
-// });
-
 app.get("/", (req, res) => {
   console.log("app.get")
     res.send("Hello World");
